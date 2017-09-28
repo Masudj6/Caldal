e615ac9e4065ccebf2fadf24e1d8f209abc718d8
masudj <mozibul15-7993@diu.edu.bd>
2017-09-28 12:14:07 +0600

    1/desing login & signup

--- app/src/main/java/com/sk/chaldal/Login_Activity/Mobile_Login_Activity.java
+++ app/src/main/java/com/sk/chaldal/Login_Activity/Mobile_Login_Activity.java
@@ -1,15 +1,42 @@
 package com.sk.chaldal.Login_Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.sk.chaldal.R;
 
-public class Mobile_Login_Activity extends AppCompatActivity {
+public class Mobile_Login_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mobile_phone, pin_in_sms, login_option;
+    private EditText phone;
+    private Button otp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile__login_);
+
+        ini();
+    }
+
+
+    private void ini() {
+        mobile_phone= (TextView) findViewById(R.id.mobile_phone);
+        pin_in_sms= (TextView) findViewById(R.id.pin_in_sms);
+        login_option= (TextView) findViewById(R.id.login_option);
+        phone= (EditText) findViewById(R.id.mobile_number);
+        otp= (Button) findViewById(R.id.otp);
+
+        otp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
---
+++ app/src/main/java/com/sk/chaldal/Login_Activity/Sign_Up.java
@@ -0,0 +1,15 @@
+package com.sk.chaldal.Login_Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sk.chaldal.R;
+
+public class Sign_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__up);
+    }
+}
